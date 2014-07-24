3d01b5524d382d5677d8349b483707391dc27f73
AngrySnout <splintersal@gmail.com>
2014-07-24 03:12:14 +0300

    Website preview

---
+++ Home - [happy] Clan Website_files/scripts.js
@@ -0,0 +1,19 @@
+/*
+function update()
+{
+	var a = document.getElementById('floater');
+	var psi = a.style.left;
+	psi = parseInt(psi);
+	var tg = 10 + 100*undermouse;
+	tg = tg>psi? Math.ceil((tg-psi)/5.0): Math.floor((tg-psi)/5.0);
+	a.style.left = (psi+tg) + 'px';
+}
+
+function init()
+{
+	setInterval(update, 20);
+	var a = document.getElementById('floater');
+	a.style.left = (10 + 100*undermouse) + 'px';
+	a.style.visibility = 'visible';
+}
+*/
